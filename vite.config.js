748d5707137332377b5fc4fdb6d79c5bf7ed5a86
Vincent Payet <vincentp9@outlook.fr>
2024-12-03 12:21:07 +0400

    test vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vincentp974.github.io/', // Remplacez par le nom de votre dépôt GitHub
+  build: {
+    outDir: 'dist', // Assurez-vous que la sortie correspond à votre déploiement
+  },
 })
